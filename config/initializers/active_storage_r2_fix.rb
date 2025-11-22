80b15698adf7531e294c11247af668d82073b3cc
FelipeSysten <systen3@gmail.com>
2025-11-22 06:37:38 -0300

    checksum retirar e tratar

--- config/initializers/active_storage_r2_fix.rb
+++ config/initializers/active_storage_r2_fix.rb
@@ -1,31 +1,22 @@
 # config/initializers/active_storage_r2_fix.rb
 
 # Este código corrige o erro "Aws::S3::Errors::InvalidRequest: You can only specify one non-default checksum at a time."
-# que ocorre ao usar o Active Storage com o Cloudflare R2 (ou outros serviços S3 que não suportam o checksum padrão do Rails).
-# Ele desabilita o cálculo e envio do checksum no momento do upload.
+# que ocorre ao usar o Active Storage com o Cloudflare R2.
+# Ele desabilita o cálculo e envio do checksum no momento do upload, garantindo compatibilidade.
 
-Rails.application.config.to_prepare do
-  if ActiveStorage::Blob.service.is_a?(ActiveStorage::Service::S3Service)
-    ActiveStorage::Service::S3Service.class_eval do
-      def upload(key, io, checksum: nil, **options)
-        instrument :upload, key: key, checksum: checksum do
-          begin
-            client.put_object(
-              bucket: bucket,
-              key: key,
-              body: io,
-              content_md5: checksum,
-              content_type: options[:content_type],
-              content_disposition: options[:content_disposition],
-              content_encoding: options[:content_encoding],
-              cache_control: options[:cache_control],
-              metadata: options[:metadata],
-              acl: options[:acl],
-              **options.except(:content_type, :content_disposition, :content_encoding, :cache_control, :metadata, :acl)
-            )
-          rescue Aws::S3::Errors::BadDigest
-            raise ActiveStorage::IntegrityError
-          end
+ActiveSupport.on_load(:active_storage_blob) do
+  ActiveStorage::Service::S3Service.class_eval do
+    # Sobrescreve o método de upload para remover o parâmetro de checksum que causa o erro no R2.
+    # A implementação original pode ser encontrada na documentação do Rails ou no código-fonte.
+    # Esta versão é simplificada para focar na remoção do problema.
+    def upload(key, io, checksum: nil, **options)
+      instrument :upload, key: key, checksum: checksum do
+        begin
+          # A chamada para put_object é modificada para não passar o checksum_algorithm
+          # e depender apenas do content_md5, que é o comportamento esperado.
+          object_for(key).put(body: io, content_md5: checksum, **options)
+        rescue Aws::S3::Errors::BadDigest
+          raise ActiveStorage::IntegrityError
         end
       end
     end

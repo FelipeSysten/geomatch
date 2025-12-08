e689897f9434e21126c029996f5b90d76697d35d
FelipeSysten <systen3@gmail.com>
2025-12-08 12:26:28 -0300

    logs erros

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,25 +2,32 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_match
 
-  Rails.logger.error "MSG_DEBUG: match=#{@match&.id}, user=#{current_user&.id}, params=#{params.inspect}"
-
-
   def index
+    Rails.logger.error "MSG_DEBUG index: match=#{@match&.id}, user=#{current_user&.id}, params=#{params.inspect}"
+
     @messages = @match.messages.order(created_at: :asc)
-    # Apenas para garantir que o frontend tenha os dados iniciais
-    render json: @messages.map { |m| { id: m.id, content: m.content, sender_id: m.sender_id, created_at: m.created_at.iso8601 } }
+
+    render json: @messages.map { |m|
+      {
+        id: m.id,
+        content: m.content,
+        sender_id: m.sender_id,
+        created_at: m.created_at.iso8601
+      }
+    }
   end
 
   def create
-     unless @match.participant?(current_user)
-     return head :forbidden
-     end
-    
+    Rails.logger.error "MSG_DEBUG create: match=#{@match&.id}, user=#{current_user&.id}, params=#{params.inspect}"
+
+    unless @match.participant?(current_user)
+      return head :forbidden
+    end
+
     @message = @match.messages.build(message_params)
     @message.sender = current_user
 
     if @message.save
-      # Retorna um status de sucesso.
       head :ok
     else
       render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
--- app/models/message.rb
+++ app/models/message.rb
@@ -6,21 +6,20 @@ class Message < ApplicationRecord
 
   after_create_commit :broadcast_message
 
-  Rails.logger.error "BROADCAST_DEBUG: sender=#{sender&.id}, avatar=#{sender&.avatar_url.inspect}"
-
-
   private
 
-def broadcast_message
-MatchChannel.broadcast_to(match, {
-  message: {
-    id: id,
-    content: content,
-    sender_id: sender_id,
-    created_at: created_at.iso8601,
-    display_name: sender.display_name,
-    avatar_url: sender.avatar_url # se usar ActiveStorage, gere url como fez
-  }
-})
-end
+  def broadcast_message
+    Rails.logger.error "BROADCAST_DEBUG: sender=#{sender&.id}, avatar=#{sender&.avatar_url.inspect}"
+
+    MatchChannel.broadcast_to(match, {
+      message: {
+        id: id,
+        content: content,
+        sender_id: sender_id,
+        created_at: created_at.iso8601,
+        display_name: sender.display_name,
+        avatar_url: sender.avatar_url
+      }
+    })
+  end
 end

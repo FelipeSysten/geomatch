f6daedd48d4b2d8ca4a03a99b6e216b42b85a67e
FelipeSysten <systen3@gmail.com>
2025-12-12 00:34:52 -0300

    menseger correto 10

--- db/migrate/20251212024151_add_channel_hash_to_solid_cable_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddChannelHashToSolidCableMessages < ActiveRecord::Migration[8.1]
-   def change
-    add_column :solid_cable_messages, :channel_hash, :string, null: false, default: ""
-    add_index :solid_cable_messages, :channel_hash
-  end
-end

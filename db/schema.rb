f26aadf39b4ecc82ba9ff8ca8ff3bc264932835f
FelipeSysten <systen3@gmail.com>
2025-12-11 23:47:43 -0300

    menseger correto 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_08_204521) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_024151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -146,10 +146,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_08_204521) do
   end
 
   create_table "solid_cable_messages", force: :cascade do |t|
-    t.binary "channel", null: false
-    t.bigint "channel_hash", null: false
+    t.text "channel"
+    t.string "channel_hash", default: "", null: false
     t.datetime "created_at", null: false
-    t.binary "payload", null: false
+    t.text "payload"
+    t.datetime "updated_at", null: false
     t.index ["channel"], name: "index_solid_cable_messages_on_channel"
     t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
     t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
